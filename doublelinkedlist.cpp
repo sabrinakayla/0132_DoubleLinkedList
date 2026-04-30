4bd74928b19d99803374048b3872dd59deb284d7
sabrinakayla <sabrinakuliah10@gmail.com>
2026-04-30 12:59:26 +0700

    alokasi memori

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -26,3 +26,8 @@ public:
     int nim;
     cout << "\nEnter the roll number of the student: ";
     cin >> nim;
+
+    // Step 1: Allocate memory for new node
+    Node *newNode = new Node();
+
+    
