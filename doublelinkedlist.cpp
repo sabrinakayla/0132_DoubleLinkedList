c52ce6315d017f2833960a16a9ecd3c76646b09d
sabrinakayla <sabrinakuliah10@gmail.com>
2026-04-30 12:49:55 +0700

    menambahkan class node

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -2,8 +2,21 @@
 #include <string>
 using namespace std;
 
-struct Node {
-    string judul;
-    string penyanyi;
-    Node* next;
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+    Node *prev;
 };
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
